0b75f91ec77dba1851e5c574a9b9f3e8f772ca58
awvaarua <ganstersb2@gmail.com>
2018-03-09 20:53:44 +0100

    Add post

--- messages.js
+++ messages.js
@@ -3,3 +3,7 @@ messaging.onMessage(function(payload) {
     if (!payload.data) return;
     document.querySelector('#content').innerHTML = JSON.stringify(payload);
 });
+
+// navigator.serviceWorker.addEventListener('message', function(event) {
+//     //console.log('Received a message from service worker while idle: ', event.data);
+// });
