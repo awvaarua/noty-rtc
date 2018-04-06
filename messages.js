29b6166adcc333ef9e27a7e2be590ab487280597
Jose Manuel Alfaya <jose.alfaya@TOOLFACTORY.NET>
2018-04-06 11:43:22 +0200

    Notification local fix link

--- messages.js
+++ messages.js
@@ -1,9 +1,33 @@
 messaging.onMessage(function(payload) {
-    console.log("Message received. ", payload);
-    if (!payload.data) return;
-    document.querySelector('#content').innerHTML = JSON.stringify(payload);
+    //console.log("Message received. ", payload);
+    if (payload.notification)
+        showLocalNotification(payload.notification);
+
+    if (payload.data)
+        processMessage(payload.data);
 });
 
-// navigator.serviceWorker.addEventListener('message', function(event) {
-//     //console.log('Received a message from service worker while idle: ', event.data);
-// });
+navigator.serviceWorker.addEventListener('message', function(event) {
+    //console.log('Received a message from service worker while idle: ', event.data);
+    if (event.data.data)
+        processMessage(event.data.data);
+});
+
+function processMessage(data) {
+    //Call projects implementations
+    document.querySelector('#content').innerHTML = JSON.stringify(data);
+}
+
+function showLocalNotification(data) {
+    //Assuming we have user permission
+    var options = {
+        body: data.body + "(local)",
+        icon: data.icon || "https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/32/envelope-o.png"
+    };
+    var notification = new Notification(data.title, options);
+    notification.onclick = function(event) {
+        event.preventDefault();
+        window.open(data.click_action, '_blank');
+        notification.close();
+    }
+}
