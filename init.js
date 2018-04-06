4b7ea1720a57dd53aff2072f37418da63e9d2c4a
Sebastià Sansó <sebastian.sanso@TOOLFACTORY.NET>
2018-04-06 12:13:56 +0200

    Susbcribe testing

--- init.js
+++ init.js
@@ -5,30 +5,30 @@ var config = {
     messagingSenderId: "22493502646"
 };
 firebase.initializeApp(config);
+// firebase.deleteInstanceId();
 const messaging = firebase.messaging();
 messaging.requestPermission()
     .then(function() {
-    messaging.getToken()
-        .then(function(currentToken) {
-            if (currentToken) {
-                console.log(currentToken);
-                //sendTokenToServer(currentToken);
-            } else {
-                console.log('No Instance ID token available. Request permission to generate one.');
-                // Show permission UI.
-                updateUIForPushPermissionRequired();
-                setTokenSentToServer(false);
-            }
-        })
-        .catch(function(err) {
-            console.log('An error occurred while retrieving token. ', err);
-            //showToken('Error retrieving Instance ID token. ', err);
-            //setTokenSentToServer(false);
-        });
-})
-.catch(function(err) {
-  console.log('Unable to get permission to notify.', err);
-});
+        messaging.getToken()
+            .then(function(currentToken) {
+                if (currentToken) {
+                    console.log('Generated token:' + currentToken);
+                    //sendTokenToServer(currentToken);
+                    subscribeTokenToTopic(currentToken, 'testing');
+                    subscribeTokenToTopic(currentToken, 'messaging_LIS_PT_TA');
+                } else {
+                    console.log('No Instance ID token available. Request permission to generate one.');
+                }
+            })
+            .catch(function(err) {
+                console.log('An error occurred while retrieving token. ', err);
+                //showToken('Error retrieving Instance ID token. ', err);
+                //setTokenSentToServer(false);
+            });
+    })
+    .catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+    });
 
 // Callback fired if Instance ID token is updated.
 messaging.onTokenRefresh(function() {
@@ -47,3 +47,17 @@ messaging.onTokenRefresh(function() {
       showToken('Unable to retrieve refreshed token ', err);
     });
 });
+
+//  This MUST be on server
+function subscribeTokenToTopic(token, topic) {
+    fetch('https://iid.googleapis.com/iid/v1/'+token+'/rel/topics/'+topic, {
+        method: 'POST',
+        headers: new Headers({ 'Authorization': 'key=AAAABTy3nLY:APA91bFPPBWiMWGxXz6PZyeDVi6-UPLL9I5nofWNrMrkW4KPgx64PQYGoKIcFKIRHOhyofyzhkSeWtCV-AiRvFpYYsIkAkdxqgRvxK5xAzn00DIE9H1v0Oobf6hf_4gG0U8R_Xb6dZl2' })
+    }).then(response => {
+        if (response.status < 200 || response.status >= 400)
+            throw 'Error subscribing to topic: '+response.status + ' - ' + response.text();
+        console.log('Subscribed to "'+topic+'"');
+    }).catch(error => {
+        console.error(error);
+    });
+}
